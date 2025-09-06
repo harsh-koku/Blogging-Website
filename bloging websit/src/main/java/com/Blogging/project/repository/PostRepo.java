f0a21f9dfc9f26d3bdda27eaced291efde962ca0
harsh-koku <harshkoku1@gmail.com>
2025-09-06 11:21:33 +0530

    Delete bloging websit directory

--- bloging websit/src/main/java/com/Blogging/project/repository/PostRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Blogging.project.repository;
-
-import com.Blogging.project.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepo extends JpaRepository<Post,Integer> {
-}
